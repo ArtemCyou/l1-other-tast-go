d25b7d6047e5a7570ca6b8a413e26e0248718a37
Артём Прилуцких <prilutskikh@gmail.com>
2022-06-20 16:55:49 +0300

    add 17

--- 16.go
+++ 16.go
@@ -17,15 +17,34 @@ func main() {
 	arrA := []int{4, 2, 3, 1, 5}
 	fmt.Println("Неотсортированный массив: ", arrA)
 
-	//реализация быстрой сортировки go
+	//реализация быстрой сортировки go (Быстрая сортировка, сортировка Хоара основана на принципе разделяй и властвуй)
 	quickSort(arrA, 0, len(arrA)-1)
 	fmt.Printf("Отсортированный массив (quickSort): %v\n\n", arrA)
 
-	//сортировка встроенными методами языка
+	//сортировка встроенными методами языка из пакета sort
 	otherSort(arrA)
 }
 
-func quickSort(arr []int, low, high int)  {
+func otherSort(arrA []int) {
+	//для сортировки использую встроенный метод sort.Ints()
+	sort.Ints(arrA) //так-же можно отсортировать string, float
+	fmt.Println("Отсортированный массив встроенным методом языка: ", arrA)
+
+	fmt.Println("------Сортировка своего типа-------")
+
+	//Если нужно отсортировать свой тип, воспользуемся методом sort.Slice
+	arrB := []myType{{"Бернар Арно", 3}, {"Джефф Безос", 1}, {"Илон Маск", 2}}
+	fmt.Println("Неотсортированный массив: ", arrB)
+
+	//метод принимает массив для сортировки и функцию
+	sort.Slice(arrB, func(i, j int) bool {
+		return arrB[i].place < arrB[j].place
+	})
+
+	fmt.Println("Отсортированный массив встроенным методом языка: ", arrB)
+}
+
+func quickSort(arr []int, low, high int) {
 	if low < high {
 
 		// Находим индекс в массиве для разворота массива
@@ -35,11 +54,11 @@ func quickSort(arr []int, low, high int)  {
 		// для сортировки левой и правой частей массива
 		// в соответствии с положением pivot
 
+		//рекурсивно вызываем функцию
 		// левая часть массива
 		quickSort(arr, low, pivot)
-
 		// правая часть массива
-		quickSort(arr, pivot + 1, high)
+		quickSort(arr, pivot+1, high)
 	}
 }
 
@@ -80,23 +99,4 @@ func part(arr []int, low, high int) int {
 
 	// Возвращаем позицию индекса pivot
 	return j
-}
-
-func otherSort(arrA []int)  {
-	//для сортировки использую встроенный метод sort.Ints()
-	sort.Ints(arrA) //так-же можно отсортировать string, float
-	fmt.Println("Отсортированный массив встроенным методом языка: ", arrA)
-
-	fmt.Println("------Сортировка своего типа-------")
-
-	//Если нужно отсортировать свой тип, воспользуемся методом sort.Slice
-	arrB := []myType{{"Бернар Арно", 3}, {"Джефф Безос", 1}, {"Илон Маск", 2}}
-	fmt.Println("Неотсортированный массив: ", arrB)
-
-	//метод принимает массив для сортировки и функцию
-	sort.Slice(arrB, func(i, j int) bool {
-		return arrB[i].place < arrB[j].place
-	})
-
-	fmt.Println("Отсортированный массив встроенным методом языка: ", arrB)
 }
